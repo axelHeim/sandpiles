89f9df5e76d6d5a296d909da43c64f1f0ef3588d
Axel <aheimeroth@gmx.de>
2020-03-04 12:06:18 +0100

    Datenerfassung wieder auf lists umgestellt, die am ende in ein Dataframe eingelesen werden

--- 2D/2D_exponents_dataSimulation.R
+++ 2D/2D_exponents_dataSimulation.R
@@ -30,7 +30,7 @@ perform_avalanches <- function(lattice){
         if(lattice[i,j] > z_crit){
           
           actual_lin_dis <- sqrt((i - r_0[1])^2 + (j - r_0[2])^2)# distance between r_0 actual critical point
-          avalance_lin_size <- max(1, max(avalance_lin_size, actual_lin_dis))
+          avalance_lin_size <- max(0.1, max(avalance_lin_size, actual_lin_dis))
           
 
           tmp_lattice[i,j] <- tmp_lattice[i,j] - 4
@@ -74,13 +74,19 @@ perform_avalanches <- function(lattice){
 library(plot.matrix)
 lattice_size <- 40
 z_crit <- 8      # avalanche condtion
-time_steps <- 5*1e6  # 3*1e7 lasts > 25min with lattice_size=10
+time_steps <- 5*1e4  # 3*1e7 lasts > 25min with lattice_size=10
 
 lattice <- matrix(0, nrow = lattice_size, ncol = lattice_size)     # the main lattice for the simulation
 
-simulation_data <- data.frame(s=numeric(0),t=numeric(0),l=integer(0))
+
 
 perturbat_conser <- TRUE  # perturbation conservative (TRUE) or non-conservative (FALSE)
+
+
+s_values <- c()  # list of avalanche sizes
+t_values <- c()  # list of avalance lifetimes
+l_values <- c()  # list of avalance linear sizes
+
 
 
 for(t in 1:time_steps){
@@ -88,11 +94,10 @@ for(t in 1:time_steps){
   lattice <- matrix(tmp[1:lattice_size^2], nrow = lattice_size, ncol = lattice_size)
   
   if(tmp[lattice_size^2 + 1] > 0){   # this cond. checks whether there was an avalanche at all
-    simulation_data <- rbind(simulation_data, data.frame(s=tmp[lattice_size^2 + 1],
-                                                         t=tmp[lattice_size^2 + 2],
-                                                         l=tmp[lattice_size^2 + 3]))
-    }
-  
+    s_values[[length(s_values) + 1]] <- tmp[lattice_size^2 + 1]  
+    t_values[[length(t_values) + 1]] <- tmp[lattice_size^2 + 2] 
+    l_values[[length(l_values) + 1]] <- tmp[lattice_size^2 + 3]  
+  }
   
   ## randomly place one grain of sand by z<-z+1 on random pos.
   x_rand <- sample(1:lattice_size, 1) 
@@ -130,6 +135,8 @@ for(t in 1:time_steps){
   
 }
 
+
+simulation_data <- data.frame(s=s_values,t=t_values,l=l_values)
 
 ###
 
